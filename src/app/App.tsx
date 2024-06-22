5dda1e066fe993ca0b3d3dbfd38bb9134d067350
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-06-22 20:27:11 +0300

    chore: add app template

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+import Menu from "../components/Menu";
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
