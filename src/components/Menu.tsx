67d9ce4e49c2cd0dcc36b29a81f6797c48386788
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-06-22 23:29:43 +0300

    chore: change name CSSProperties

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,23 +7,23 @@ import {GiPhone} from "react-icons/gi";
 import {FaRProject, FaTeamspeak} from "react-icons/fa";
 
 
-const scrollStyle1 = {'--i': '1'} as React.CSSProperties;
-const scrollStyle2 = {'--i': '2'} as React.CSSProperties;
-const scrollStyle3 = {'--i': '3'} as React.CSSProperties;
-const scrollStyle4 = {'--i': '4'} as React.CSSProperties;
-const scrollStyle5 = {'--i': '5'} as React.CSSProperties;
-const scrollStyle6 = {'--i': '6'} as React.CSSProperties;
+const style1 = {'--i': '1'} as React.CSSProperties;
+const style2 = {'--i': '2'} as React.CSSProperties;
+const style3 = {'--i': '3'} as React.CSSProperties;
+const style4 = {'--i': '4'} as React.CSSProperties;
+const style5 = {'--i': '5'} as React.CSSProperties;
+const style6 = {'--i': '6'} as React.CSSProperties;
 
 const Menu = () => {
     return (
         <div className={styles.container}>
             <ul className={styles.ul}>
-                <li style={scrollStyle6} data-text={'HOME'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><AiFillHome /></span>Home</a></li>
-                <li style={scrollStyle5} data-text={'ABOUT'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><IoIosInformationCircleOutline /></span>About</a></li>
-                <li style={scrollStyle4} data-text={'SERVICES'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><MdOutlineMiscellaneousServices /></span>Services</a></li>
-                <li style={scrollStyle3} data-text={'PORTFOLIO'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaRProject /></span>Portfolio</a></li>
-                <li style={scrollStyle2} data-text={'TEAM'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaTeamspeak /></span>Team</a></li>
-                <li style={scrollStyle1} data-text={'CONTACTS'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><GiPhone /></span>Contacts</a></li>
+                <li style={style6} data-text={'HOME'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><AiFillHome /></span>Home</a></li>
+                <li style={style5} data-text={'ABOUT'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><IoIosInformationCircleOutline /></span>About</a></li>
+                <li style={style4} data-text={'SERVICES'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><MdOutlineMiscellaneousServices /></span>Services</a></li>
+                <li style={style3} data-text={'PORTFOLIO'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaRProject /></span>Portfolio</a></li>
+                <li style={style2} data-text={'TEAM'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaTeamspeak /></span>Team</a></li>
+                <li style={style1} data-text={'CONTACTS'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><GiPhone /></span>Contacts</a></li>
             </ul>
         </div>
     );
