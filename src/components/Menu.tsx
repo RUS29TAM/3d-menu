88dcbd718e79de8136717f0506073321616b2f5d
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-06-23 01:03:57 +0300

    feat: add mirror menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,6 +14,13 @@ const style4 = {'--i': '4'} as React.CSSProperties;
 const style5 = {'--i': '5'} as React.CSSProperties;
 const style6 = {'--i': '6'} as React.CSSProperties;
 
+const style7 = {'--i': '7'} as React.CSSProperties;
+const style8 = {'--i': '8'} as React.CSSProperties;
+const style9 = {'--i': '9'} as React.CSSProperties;
+const style10 = {'--i': '10'} as React.CSSProperties;
+const style11 = {'--i': '11'} as React.CSSProperties;
+const style12 = {'--i': '12'} as React.CSSProperties;
+
 const Menu = () => {
     return (
         <div className={styles.container}>
@@ -24,6 +31,14 @@ const Menu = () => {
                 <li style={style3} data-text={'PORTFOLIO'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaRProject /></span>Portfolio</a></li>
                 <li style={style2} data-text={'TEAM'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><FaTeamspeak /></span>Team</a></li>
                 <li style={style1} data-text={'CONTACTS'} className={styles.li}><a className={styles.a} href={'##'}><span className={styles.span}><GiPhone /></span>Contacts</a></li>
+            </ul>
+            <ul className={styles.ul2}>
+                <li style={style12} data-text={'HOME'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><AiFillHome /></span>Home</a></li>
+                <li style={style11} data-text={'ABOUT'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><IoIosInformationCircleOutline /></span>About</a></li>
+                <li style={style10} data-text={'SERVICES'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><MdOutlineMiscellaneousServices /></span>Services</a></li>
+                <li style={style9} data-text={'PORTFOLIO'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><FaRProject /></span>Portfolio</a></li>
+                <li style={style8} data-text={'TEAM'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><FaTeamspeak /></span>Team</a></li>
+                <li style={style7} data-text={'CONTACTS'} className={styles.li2}><a className={styles.a2} href={'##'}><span className={styles.span2}><GiPhone /></span>Contacts</a></li>
             </ul>
         </div>
     );
